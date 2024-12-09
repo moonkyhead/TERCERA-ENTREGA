9a5dfded1677936c6a98970c7b77d4c93d625036
leandro <leandro1979@hotmail.com>
2024-12-09 17:10:07 -0300

    arreglando codigo en url.py

--- src/curso/urls.py
+++ src/curso/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, about  # Asegúrate de importar la vista `about`
 
 app_name = "curso"
 
 urlpatterns = [
-path("", index, name="index"),
-path("", about/, about, name="about"),
+    path("", index, name="index"),
+    path("about/", about, name="about"),  # Ruta corregida para la página "about"
 ]
