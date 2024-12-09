fc422605240883932218f3893a7c9f22f76195f5
leandro <leandro1979@hotmail.com>
2024-12-09 20:18:31 -0300

    configurando vistas y urls

--- src/curso/urls.py
+++ src/curso/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 
 from .views import index, about 
 
+from .import views
+
+
 app_name = "curso"
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
+    path("autor/", views.crear_autor, name="crear_autor"),
+    path("libros/", views.LibroListView.as_view(), name="libros"),
+    path("libros/"<int:pk>/, views.LibroDetailView.as_view(), name="libro_detalle"),
+    path("libros/"<int:pk>/update/, views.LibroUpdateView.as_view(), name="libro_update"),
+    path("libros/"<int:pk>/delete/, views.LibroDeleteView.as_view(), name="libro_delete"),
 ]
+
--- src/curso/views.py
+++ src/curso/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AutorForm, LibroiForm, EditorialForm
+
 
 # Create your views here.
 def index(request):
@@ -8,3 +10,43 @@ def about(request):
     return render(request, 'curso/about.html')
 
                   
+def crear_autor(request):
+    if request.method == "POST":
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = AutorForm()
+        return render(request, 'curso/crear_autor.html', {'form': form})
+    
+    
+def crear_libro(request):
+    if request.method == "POST":
+        form = LibroiForm(request.POST)
+        if form.is_valid():
+            form.save
+            return redirect('crear_libro')
+        
+        else:
+            form = LibroiForm()
+            return render(request, 'curso/crear_libro.html', {'form': form})
+        
+        
+def crear_editorial(request):
+    if request.method == "POST":
+        form = EditorialForm(request.POST)
+        if form.is_valid():
+            form.save
+            return redirect('crear_editorial.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
