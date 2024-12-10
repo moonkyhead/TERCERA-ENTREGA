0712916b1fcf83778edb0f5ace20e461db9960fd
leandro <leandro1979@hotmail.com>
2024-12-10 16:07:47 -0300

    creando nuevas plantillas

--- src/curso/urls.py
+++ src/curso/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path("autor/", views.crear_autor, name="crear_autor"),
     path("libros/", views.crear_libro, name="crear_libro"),
     path("editorial/", views.crear_editorial, name="crear_editorial"),
+    path('Autor/', views.crear_autor, name='crear_autor'),
 ]
 
--- src/curso/views.py
+++ src/curso/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import AutorForm, LibroForm, EditorialForm
-from . models import Autor, Libro, Editorial
+from .models import Autor, Libro, Editorial
 
 
-# Create your views here.
 def index(request):
     return render(request, 'curso/index.html')
+
 
 def about(request):
     return render(request, 'curso/about.html')
 
-                  
+
 def crear_autor(request):
     if request.method == "POST":
         form = AutorForm(request.POST)
@@ -19,39 +19,45 @@ def crear_autor(request):
             return redirect('index')
     else:
         form = AutorForm()
-        return render(request, 'curso/crear_autor.html', {'form': form})
-    
-    
+    return render(request, 'curso/crear_autor.html', {'form': form})
+
+
 def crear_libro(request):
     if request.method == "POST":
-        form = LibroiForm(request.POST)
+        form = LibroForm(request.POST)  
         if form.is_valid():
-            form.save
-            return redirect('crear_libro')
-        
-        else:
-            form = LibroiForm()
-            return render(request, 'curso/crear_libro.html', {'form': form})
-        
-        
+            form.save()  
+            return redirect('crear_libro')  
+    else:
+        form = LibroForm()  
+    return render(request, 'curso/crear_libro.html', {'form': form})
+
+
 def crear_editorial(request):
     if request.method == "POST":
         form = EditorialForm(request.POST)
         if form.is_valid():
-            form.save
-            return redirect('crear_editorial.html', {'form': form})
+            form.save() 
+            return redirect('crear_editorial')  
+    else:
+        form = EditorialForm()
+    return render(request, 'curso/crear_editorial.html', {'form': form})
 
 
 def Autor_list(request):
-    query = curso.Autor.All()
-    return render(request, 'Autor/Autor_list.html')
+    autores = Autor.objects.all()  
+    return render(request, 'Autor/Autor_list.html', {'autores': autores})
 
 
 def Libro_list(request):
-    return render(request, 'Libro/Libro_list.html')
+    libros = Libro.objects.all()  
+    return render(request, 'Libro/Libro_list.html', {'libros': libros})
+
 
 def Editorial_list(request):
-    return render(request, 'Editorial/Editorial_list.html')
+    editoriales = Editorial.objects.all()  
+    return render(request, 'Editorial/Editorial_list.html', {'editoriales': editoriales})
+
 
 
 
