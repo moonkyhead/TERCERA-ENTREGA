e6c37a3d9d81c964c2fc57e7a753b08b8c8170c0
leandro <leandro1979@hotmail.com>
2024-12-10 12:08:21 -0300

    solucionado el problema con Editorial, estaba mal la definicion de Libro

--- src/curso/admin.py
+++ src/curso/admin.py
@@ -10,14 +10,16 @@ class AutorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'nacionalidad', 'descripcion')
     search_fields = ('nombre', 'apellido')
 
-
 @admin.register(Libro)
 class LibroAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'fecha_publicacion', 'autor')
     search_fields = ('titulo', 'autor')
-    
-    
+
 @admin.register(Editorial)
 class EditorialAdmin(admin.ModelAdmin):
-    list_display = ('nombre','libros')
-    search_fields = ('nombre','libros')
+    list_display = ('nombre', 'mostrar_libros')  
+    search_fields = ('nombre',)
+
+    def mostrar_libros(self, obj):
+        return ", ".join([libro.titulo for libro in obj.libros.all()])
+    mostrar_libros.short_description = 'Libros'  
