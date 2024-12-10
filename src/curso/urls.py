ed931e696daa76fd95460360fb668347a447de34
leandro <leandro1979@hotmail.com>
2024-12-09 22:35:17 -0300

    creating super user admin

--- src/curso/urls.py
+++ src/curso/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from .views import index, about 
-
 from .import views
+from django.contrib import admin
 
 
 app_name = "curso"
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("", index, name="index"),
     path("about/", about, name="about"),
     path("autor/", views.crear_autor, name="crear_autor"),
